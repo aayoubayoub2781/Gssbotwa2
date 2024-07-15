424a8055884506475387cc01fdb976842b9b452c
aayoubayoub2781 <148594510+aayoubayoub2781@users.noreply.github.com>
2024-07-15 15:00:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ const chalk = require('chalk');
 
 
 // Other
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.link = 'https://chat.whatsapp.com/Kj0QvCzZQ6mBflhckX64ak'
+global.linkGroup = 'https://chat.whatsapp.com/Kj0QvCzZQ6mBflhckX64ak'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "Ayoub";
+global.owner = ['212671648947'];
+global.premium = [212671648947'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "Ayoub bot √";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
@@ -21,28 +21,28 @@ global.autoread = false
 global.available = false
 global.autoTyping = false
 global.autoRecord = false
-global.autoreact = false
-global.onlygroup = false
+global.autoreact = true
+global.onlygroup = true
 global.onlypc = false
 global.welcome = true
 global.autoBlock = true
  
 /*REPLY MESSAGE*/
 global.mess = {
-    banned: '`You are *Banned* fron using commands!`',
+    banned: '`سير تلعب نتا معمرك متبقا تخدم بيا انا!`',
     bangc: '`This Group is *Banned* from using Commands!`',
     success: '`✓ Success`',
-    admin: '`This feature is only for group admins`',
-    botAdmin: '`I am not an admin!`',
-    owner: '`You are not my owner`',
+    admin: '`سير تلعب حتا تكون ادمين `',
+    botAdmin: '`انا ماشي ادمين!`',
+    owner: '`فين كانعرفك `',
     group: '`You can use this command only in groups ❌`',
-    private: '`Feature is used only for private chats!`',
+    private: '`سير تلعب!`',
     bot: '`Bot number user special features`',
-    wait: '*Processing Your request*',
+    wait: '*اودي صبر عليا راني معصبt*',
     endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
 };
 
 global.limitawal = {
     premium: 'Infinity',
-    free: 10
+    free: 90000000000000000000
 };
